85e837cf56611faab80b5cc8a8edc0b2d9f93727
Nicolas Torres <nicolasparratorres@gmail.com>
2025-05-05 00:22:50 -0500

    deployment

--- crawler_api/crawler.py
+++ crawler_api/crawler.py
@@ -15,4 +15,4 @@ async def root():
 def run_app():
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("crawler_api.crawler:app", host="0.0.0.0", port=port)
+    uvicorn.run("crawler_api.crawler:app", host="0.0.0.0", port=port, log_level="info")
